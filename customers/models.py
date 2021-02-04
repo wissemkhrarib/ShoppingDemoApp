44afc8e7d61b9ed1f0e1f483321beef68b75cd6b
wissemkhrarib <khraribwissem@gmail.com>
2021-02-04 03:00:58 -0600

    Customer model created

---
+++ customers/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    address = models.CharField(max_length=2000)
+    phone_number = models.CharField(max_length=8)
